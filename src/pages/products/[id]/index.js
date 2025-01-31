cb413f0f81746f800f0ef62d27f48f93e6b57520
Danu Prasetyo <danu.prasetyo@digiform.co.id>
2025-01-31 13:52:56 +0700

    useSWR(stale while revalidate)

--- src/pages/products/[id]/index.js
+++ src/pages/products/[id]/index.js
@@ -1,17 +1,45 @@
 import { formatCurrency } from "@/helpers/util/formatCurrency";
 import { getProductById } from "@/services/products";
+import axios from "axios";
 import React from "react";
+import useSWR from "swr";
 
+/** useSWR(Stale while revalidate) : hooks third party dari tim vercel untuk fetching data, caching dan revalidate di sisi klien
+ * rumus : const { data, error, isLoading, isValidating } = useSWR(key(endpoint), dataFetcher)
+ * srw pnya beberapa properti
+ * data : Data yang diambil dari API
+ * error : error handling saat ambil data
+ * isLoading: status loading
+ * isValidating : status validasi ulang data(perbarui data)
+ */
 const ProductDetailPage = ({ detailProduct }) => {
+  const api = process.env.NEXT_PUBLIC_API;
+
+  const { data, error, isLoading, isValidating } = useSWR(
+    `${api}/products/${detailProduct?.id}`,
+    async () => {
+      const res = await axios.get(`${api}/products/${detailProduct?.id}`);
+      return res.data;
+    },
+    {
+      initialData: detailProduct,
+      refreshInterval: 1000,
+    }
+  );
+
+  if (error) return <div className="h-screen text-8xl text-center">Gagal mengambil data</div>;
+  if (isLoading) return <div className="h-screen text-8xl text-center">Sedang memuat data ...</div>;
+
   return (
     <>
       <div className="flex flex-col px-5 py-5 bg-gradient-to-b from-black to-blue-900 min-h-screen">
         <h1 className="text-4xl font-bold text-white">Detail Produk</h1>
         <div className="p-4 mt-5 rounded-2xl bg-white bg-opacity-20 max-w-xl">
-          <h2 className="text-2xl font-bold text-white">{detailProduct?.title}</h2>
-          <p className="text-white font-semibold mt-5">{detailProduct.description}</p>
-          <p className="text-white text-xl font-bold mt-5">{formatCurrency(detailProduct.price, "en-US", "USD")}</p>
+          <h2 className="text-2xl font-bold text-white">{data?.title}</h2>
+          <p className="text-white font-semibold mt-5">{data?.description}</p>
+          <p className="text-white text-xl font-bold mt-5">{formatCurrency(data?.price, "en-US", "USD")}</p>
         </div>
+        {isValidating && <p className="text-white font-bold text-6xl mt-4">Memperbarui data ...</p>}
       </div>
     </>
   );
